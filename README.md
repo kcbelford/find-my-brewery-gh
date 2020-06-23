effe983572441b0e39b580f53b06f18a808d54cd
kcbelford <kcbelford@hotmail.com>
2020-06-23 14:26:13 -0700

    adding updated files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Find My Brewery
+
+Every craft beer enthusiast should have a brewery they can call their own. This site was created to aid in that discovery. With the use of the Open Brewery DB API, users can search via keyword, city or brewery name to find their new favorite brewery.
+
+### View the Site
+
+https://find-my-brewery.netlify.app/
+
+### Technologies Used
+
+HTML, CSS, JavaScript, Node.js, Nuxt.js, Open Brewery DB API
---
+++ components/footer.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div class="footer">
+    <p class="footer-content">created by kevin belford using the <a href="https://www.openbrewerydb.org/" target="_blank" class="api-link">open brewery db api</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.footer {
+  background-color: #24352B;
+  text-align: center;
+  padding: 6rem 0;
+  margin-top: 4rem;
+}
+
+.footer-content {
+  color: #FFFFFF;
+  text-transform: uppercase;
+  word-spacing: 2px;
+  letter-spacing: 0.5px;
+  margin: 0;
+  font-size: 1rem;
+}
+
+.api-link {
+  color: #AFB28D;
+}
+
+.api-link:hover:visited {
+  color: #AFB28D;
+}
+
+/* mobile media query */
+@media screen and (max-width: 577px) {
+    .footer-content {
+      font-size: 0.5rem;
+    }
+
+}
+
+/* tablet media query */
+@media screen  and (max-width: 769px) {
+
+}
+</style>
